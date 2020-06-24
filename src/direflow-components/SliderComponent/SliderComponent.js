d5eaa9dc723e02c3a76c38c41c307ae4444bfe4d
John Case <jcase@symplr.com>
2020-06-24 13:12:29 -0500

    Add SliderComponent

---
+++ src/direflow-components/SliderComponent/SliderComponent.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Slider } from '@fluentui/react';
+
+const SliderComponent = (props) => {
+  const {
+  } = props;
+
+  const defaultSliderProps = {
+  };
+
+  return (
+    <Slider {...defaultSliderProps} />
+  );
+};
+
+SliderComponent.propTypes = {
+};
+
+SliderComponent.defaultProps = {
+};
+
+export default SliderComponent;
---
+++ src/direflow-components/SliderComponent/index.js
@@ -0,0 +1,12 @@
+import {
+  DireflowComponent
+} from 'direflow-component';
+import SliderComponent from './SliderComponent';
+
+export default DireflowComponent.create({
+  component: SliderComponent,
+  configuration: {
+    tagname: 'symplr-slider',
+    // useShadow: false
+  }
+});
